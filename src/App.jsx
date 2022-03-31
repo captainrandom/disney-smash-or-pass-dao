87a3ce3543bae9744636c316345ee5fbb1d69012
Mark <captainrandom@users.noreply.github.com>
2022-03-31 00:00:09 +0000

    Section 1 Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
-  return (
-    <div className="landing">
-      <h1>Hello to My DAO</h1>
-    </div>
-  );
+	console.log('hello');
+	const address = useAddress();
+	const connectWithMetamask = useMetamask();
+	console.log('address:', address);
+
+	if (!address) {
+		return (
+			<div className="landing">
+				<h1>Welcome to Disney Smash or Pass DAO</h1>
+				<button onClick={connectWithMetamask} className="btn-hero">
+					Connect your wallet
+				</button>
+			</div>
+		);
+	}
+
+	// this is the case where we have the user's address
+	// which means they've connected their wallet to our site
+	return (
+		<div className="landing">
+			<h1>wallet connected, now what!</h1>
+		</div>
+	);
 };
 
 export default App;
