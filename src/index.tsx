1b996f42952b8b44776ae72eedd7f553942a2dda
Mark Jackson <markjackson@Marks-MacBook-Pro-2.local>
2022-04-02 18:05:10 -0500

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 
 // Import thirdweb provider and Rinkeby ChainId
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Rinkeby;
 
 // Wrap your app with the thirdweb provider
 ReactDOM.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <ThirdwebProvider desiredChainId={activeChainId}>
+            <App/>
+        </ThirdwebProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
